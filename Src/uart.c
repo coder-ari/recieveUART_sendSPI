7482fd6a75653a06410d97387b289fd215439cd4
coder-ari <garitra055@gmail.com>
2025-05-30 14:31:25 +0530

    added error handling for corrupt data

--- Src/uart.c
+++ Src/uart.c
@@ -22,7 +22,9 @@ static volatile uint8_t fifo[FIFO_SIZE];
 static volatile uint16_t fifo_head = 0;
 static volatile uint16_t fifo_tail = 0;
 
-static volatile uint8_t collecting = 0;
+// Shift buffer for frame detection
+#define FRAME_SEARCH_BUFFER 8
+static uint8_t shift_buf[FRAME_SEARCH_BUFFER] = {0};
 
 // FIFO helpers
 static uint16_t fifo_available(void) {
@@ -36,6 +38,12 @@ static uint16_t fifo_free_space(void) {
 static void fifo_push(uint8_t byte) {
     fifo[fifo_head] = byte;
     fifo_head = (fifo_head + 1) % FIFO_SIZE;
+}
+
+static void fifo_push_frame(uint8_t *frame) {
+    for (int i = 0; i < CAN_FRAME_SIZE; i++) {
+        fifo_push(frame[i]);
+    }
 }
 
 static uint8_t fifo_pop(void) {
@@ -88,29 +96,41 @@ void uart_try_send_from_fifo(void) {
 }
 
 void USART1_IRQHandler(void) {
-    if (USART1_SR & (1 << 5)) {
+    if (USART1_SR & (1 << 5)) { // RXNE
         uint8_t byte = USART1_DR;
 
-        if (!collecting) {
-            if (byte == START_BYTE) {
-                collecting = 1;
-                if (fifo_free_space() > 0) fifo_push(byte);
+        // Shift buffer left
+        for (int i = 0; i < FRAME_SEARCH_BUFFER - 1; i++) {
+            shift_buf[i] = shift_buf[i + 1];
+        }
+        shift_buf[FRAME_SEARCH_BUFFER - 1] = byte;
+
+        // Check if we have a candidate frame
+        if (shift_buf[0] == START_BYTE) {
+            uint8_t sum = 0;
+            for (int i = 0; i < CAN_FRAME_SIZE - 1; i++) {
+                sum += shift_buf[i];
             }
-        } else {
-            if (fifo_free_space() > 0) {
-                fifo_push(byte);
-            } else {
-                uart_send_async_string("FIFO Overflow\r\n");
+
+            if (sum == shift_buf[CAN_FRAME_SIZE - 1]) {
+                if (fifo_free_space() >= CAN_FRAME_SIZE) {
+                    fifo_push_frame(shift_buf);
+                } else {
+                    uart_send_async_string("FIFO Full\r\n");
+                }
+
+                // Clear shift_buf after valid frame to avoid repeat detection
+                for (int i = 0; i < FRAME_SEARCH_BUFFER; i++) shift_buf[i] = 0;
             }
         }
     }
 
-    if ((USART1_SR & (1 << 7)) && (USART1_CR1 & (1 << 7))) {
+    if ((USART1_SR & (1 << 7)) && (USART1_CR1 & (1 << 7))) { // TXE
         if (uart_tx_tail != uart_tx_head) {
             USART1_DR = uart_tx_buffer[uart_tx_tail];
             uart_tx_tail = (uart_tx_tail + 1) % UART_BUFFER_SIZE;
         } else {
-            USART1_CR1 &= ~(1 << 7);  // Disable TXE
+            USART1_CR1 &= ~(1 << 7);  // Disable TXE interrupt
         }
     }
 }
